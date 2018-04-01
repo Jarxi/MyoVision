895a847116725fca84a0c29821204d155c9b77f7
Eric Yihan Chen <ericyihc@usc.edu>
2018-03-31 18:04:59 -0700

    implement get Emotion and vibrate

--- script/MyoCV.py
+++ script/MyoCV.py
@@ -3,6 +3,10 @@ import cv2
 import myo as libmyo; libmyo.init("/Users/chenyihan/Desktop/USC/hackathon/LAHacks2018/FeelTheWorld/MyoSDK/myo.framework")
 import time
 import sys
+import urllib3
+import cognitive_face as CF
+from PIL import Image
+import operator
 
 HEIGHT = 720
 WIDTH = 1280
@@ -78,22 +82,46 @@ def locateObject(image, startX, endX, startY, endY):
         print("not center")
         return 0
 
-def detectFace(image):
+def detectFace(image, emotion, myo):
     path = "/Library/Frameworks/Python.framework/Versions/3.5/lib/python3.5/site-packages/cv2/data/haarcascade_frontalface_default.xml"
     face_cascade = cv2.CascadeClassifier(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
-        locateObject(x, x + w, y, y + h)
+        cv2.putText(image, emotion, (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
 
 # vibrate the Myo band based on given interval
 def vibrate(myo, interval):
     myo.vibrate("short")
     time.sleep(float(interval))
 
+def detectEmotion(image):
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    KEY = '6f850447110646e68b24b1365173e344'
+    CF.Key.set(KEY)
+
+    BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'  # Replace with your regional Base URL
+    CF.BaseUrl.set(BASE_URL)
+    # image = image.tostring()
+    img = Image.fromarray(image, 'RGB')
+    # img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+    faces = CF.face.detect(img, face_id=True, landmarks=False, attributes='emotion')
+    for face in faces:
+        faceRectangle = face['faceRectangle']
+        x = faceRectangle['left']
+        y = faceRectangle['top']
+        w = faceRectangle['width']
+        h = faceRectangle['height']
+        emotions = face['faceAttributes']['emotion']
+        emotion = max(emotions.items(), key=operator.itemgetter(1))[0]
+        print(emotion)
+        return emotion
+
 def main():
-    target = input("What do you want to find? ")
 
     feed = libmyo.device_listener.Feed()
     hub = libmyo.Hub()
@@ -109,29 +137,62 @@ def main():
             myo.vibrate("short")
             myo.vibrate("medium")
 
-        while myo.pose != "fist": {} # wait for command
+        while 1:
+            target = input("What do you want to find? ")
+            # for limiting emotion analysis rate
 
-        # start finding
-        myo.vibrate("short")
-        myo.vibrate("short")
+            if target == 'bottle':
+                #while myo.pose != "double_tap": {} # wait for command
 
-        while cap.isOpened() and hub.running and myo.connected:
-            ret, frame = cap.read()
-            if target == 'person':
-                detectFace(frame)
-            degree = detectObject(frame, target)
-            if (degree):
-                vibrate(myo, 0.1)
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            if myo.pose == 'fingers_spread':
-                print("Quit finding")
-                break
+                # start finding
+                myo.vibrate("short")
+                myo.vibrate("short")
 
-        cap.release()
-        cv2.destroyAllWindows()
+                while cap.isOpened() and hub.running and myo.connected:
+                    ret, frame = cap.read()
+                    #if target == 'person':
+                    #    detectFace(frame)
+                    degree = detectObject(frame, target)
+                    if (degree):
+                        myo.vibrate("short")
+                    cv2.imshow('frame',frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                    if myo.pose == 'double_tap':
+                        print("Quit finding")
+                        break
 
+                cap.release()
+                cv2.destroyAllWindows()
+
+            elif target == 'emotion': 
+                counter = 0
+                emotion = 'neutral'
+                while(cap.isOpened()):
+                    ret, frame = cap.read()
+                    detectFace(frame, emotion, myo)
+                    if counter % 20 == 0:
+                        emotion = detectEmotion(frame)
+                        counter = 0
+                        if emotion == 'surprise' or emotion == 'anger':
+                            myo.vibrate('long')
+                            myo.vibrate('short')
+                            myo.vibrate('long')
+                        elif emotion == 'netural' or emotion == 'contempt':
+                            myo.vibrate('short')
+                            myo.vibrate('medium')
+                            myo.vibrate('short')
+                        else:
+                            myo.vibrate('short')
+                            myo.vibrate('medium')
+                            myo.vibrate('medium')
+                    cv2.imshow('frame',frame)
+                    counter = counter + 1
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                cap.release()
+                cv2.destroyAllWindows()
     except KeyboardInterrupt:
         print("Quitting...")
     finally:
